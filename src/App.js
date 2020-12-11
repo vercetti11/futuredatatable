dac6d5276e131b2ba84aad0ac9f74318bc7baeb8
Octavio <octavio@vercetti.vision>
2020-12-11 12:50:23 +0100

    rendering merchant logo inside an image insead of avatar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { Avatar, Table, TableBody, Tooltip } from "evergreen-ui";
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
   useEffect(() => {
     fetch(
       "https://search-api.fie.future.net.uk/widget.php?id=review&model_name=xbox_one_s&area=GB"
@@ -10,16 +11,54 @@ function App() {
       .then(res => res.json())
       .then(data => setData(data.widget.data.offers));
   }, []);
+  if (!data) return <p>Loading...</p>;
+
   return (
-    <div className="App">
-      <p>Hi</p>
-      {data.map(offer => (
-        <div style={{ marginBottom: 16 }} key={offer.id}>
-          {JSON.stringify(offer)}
-        </div>
-      ))}
-    </div>
+    <Table maxWidth="90vw" margin="auto" marginTop={40}>
+      <Table.Head>
+        <Table.TextHeaderCell>Product name</Table.TextHeaderCell>
+        <Table.TextHeaderCell>Product price</Table.TextHeaderCell>
+        <Table.TextHeaderCell>Link to merchant</Table.TextHeaderCell>
+        <Table.TextHeaderCell>Merchant name</Table.TextHeaderCell>
+        <Table.TextHeaderCell>Merchant logo</Table.TextHeaderCell>
+        <Table.TextHeaderCell>Image</Table.TextHeaderCell>
+      </Table.Head>
+      <Table.Body>
+        {data.map(offer => (
+          <TableRow
+            key={offer.id}
+            productName={offer.offer.name}
+            productPrice={`${offer.offer.price} ${offer.offer.currency_iso}`}
+            linkToMerchant={offer.merchant.url}
+            merchantName={offer.merchant.name}
+            merchantLogoURL={offer.merchant.logo_url}
+          />
+        ))}
+      </Table.Body>
+    </Table>
   );
 }
+
+const TableRow = ({
+  id,
+  productName,
+  productPrice,
+  linkToMerchant,
+  merchantName,
+  merchantLogoURL,
+}) => {
+  return (
+    <Table.Row key={id}>
+      <Table.TextCell>{productName}</Table.TextCell>
+      <Table.TextCell>{productPrice}</Table.TextCell>
+      <Table.TextCell>{linkToMerchant}</Table.TextCell>
+      <Table.TextCell>{merchantName}</Table.TextCell>
+      <Table.Cell>
+        <Avatar src={merchantLogoURL} size={40} name={merchantName} />
+      </Table.Cell>
+      <Table.TextCell>Image</Table.TextCell>
+    </Table.Row>
+  );
+};
 
 export default App;
